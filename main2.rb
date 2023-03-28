959fb315f1e8f446e2856a3cf828578d0145f5d1
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-27 22:02:31 -0400

    Board.any_empty_positions? done

--- main2.rb
+++ main2.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_reader :board, :place_mark
+  attr_reader :place_mark, :empty_positions
   @@board = ["_1_|_2_|_3_",
              "_4_|_5_|_6_",
              " 7 | 8 | 9 "]
@@ -7,6 +7,10 @@ class Board
   def initialize
     # Initialize an empty board
     @@board
+  end
+
+  def display
+    puts @@board
   end
 
   def place_mark(position, mark)
@@ -17,12 +21,13 @@ class Board
         line[spot] = "#{mark.upcase}"
       end
     end
-     puts @@board
+     
     
   end
 
   def empty_positions?
     # Check if there are any empty positions left on the board
+    @@board.any? { |line| line.match?("#{/\p{Digit}/}") }
   end
 
   def winner
@@ -31,7 +36,30 @@ class Board
 end
 
 test = Board.new
-puts test.board
+puts test.display
+
+puts test.empty_positions?
+
+test.place_mark(8, "x")
+
+puts test.display
+
+test.place_mark(1,'o')
+test.place_mark(2,'x')
+test.place_mark(3,'o')
+
+puts test.display
+
+test.place_mark(4,'o')
+test.place_mark(5,'o')
+test.place_mark(6,'o')
+
+test.place_mark(7,'x')
+test.place_mark(9,'x')
+
+test.display
+
+puts test.empty_positions?
 
 
 
