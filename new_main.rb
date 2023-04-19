9faa686ffd94a92c8de9fbbb8d47aaca487fc1b8
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-04-19 18:55:31 -0400

    fixed validation issue but u have to manually start the game over if a player's chosen mark is not accepted. added a Game class with empty methods

--- new_main.rb
+++ new_main.rb
@@ -1,65 +1,31 @@
 # GAME SEQUENCE:
 
-# ask first player if they are x or o
-def get_player_one_choice
-  puts "Player 1, would you like to be X or O?"
-  answer = gets.chomp
-  return answer.upcase!
+puts "\n***WELCOME TO TIC TAC TOE***"
 
-  # error handling -- player must input only x or o, not case sensitive. then convert to caps
-  #look up - error handling - how to start function over if necessary
-
-
-end
-
-
-
-# second player is automatically x or o
-def get_player_two_choice
-  if get_player_one_choice == 'X' 
-    return 'O'
-  else 
-    return 'X'
-  end
-
-end
-
-def show_choices
-  puts "Player 1 has chosen #{get_player_one_choice}, so Player 2 is #{get_player_two_choice}."
-end
-
-
-
-
-# a class to initialize two players per game
-
-class PlayerSet
+class PlayerSet # 2 players per game
   attr_accessor :player_one_marks, :player_two_marks
   attr_reader :player_one_choice, :player_two_choice
 
   def initialize
-    puts "Player 1, please enter x or o"
+    puts "\nPlayer 1, please enter x or o"
     @player_one_choice = validate_choice(gets.chomp)
     @player_two_choice = determine_player_two_choice(@player_one_choice)
-    puts "Player 1 has chosen #{@player_one_choice}. Player 2 is #{@player_two_choice}."
+    puts "\nPlayer 1 has chosen #{@player_one_choice}. Player 2 is #{@player_two_choice}."
     @player_one_marks = []
     @player_two_marks = []
   end
 
-  def validate_choice(choice)
+  def validate_choice(choice) # get first player's choice and only accept input if it is x or o, non-case sensitive but converts to lowercase
     choice.downcase!
-    while choice
-      case choice == gets.chomp
-      when 'x' || 'o'
-        return choice
-      else
-        puts "Please enter x or o."
-      end
+    if choice == 'x' || choice == 'o'
+      return choice
+    else
+      puts "\nYou can only choose x or o, not '#{choice}'. Please start over."
+      exit
     end
-    
   end
 
-  def determine_player_two_choice(player_one_choice)
+  def determine_player_two_choice(player_one_choice) # second player is automatically x or o based on first player's choice
     if player_one_choice == 'x'
       return "o"
     else
@@ -70,10 +36,58 @@ class PlayerSet
 end
 
 players = PlayerSet.new
-puts "Player 1 testing: #{players.player_one_choice}"
-puts "Player 2 testing: #{players.player_two_choice}"
+
 
 # show the game board
+class Game
+  attr_accessor :board
+
+  def initialize
+    @board = ["_1_|_2_|_3_",
+      "_4_|_5_|_6_",
+      " 7 | 8 | 9 "]
+  end
+
+  def place_mark(position, mark)
+    # Place a mark (X or O) on the board at the given position -- 
+    # players.player_one_choice or players.player_two_choice
+    @board.each do |line|
+      if line.include?("#{position}")
+        spot = line.index("#{position}")
+        line[spot] = "#{mark.upcase}"
+      end
+    end
+  end
+
+  def switch_player
+
+  end
+
+  def keep_playing?
+
+  end
+
+  def has_winner?
+
+  end
+
+  def announce_winner
+
+  end
+
+
+end
+
+
+puts "\nBelow is the game board. To mark a spot, enter the number where you wish to place your mark. Let the games begin! Hey-o!"
+puts "\n"
+
+
+tic_tac_toe = Game.new
+puts tic_tac_toe.board
+puts "\n"
+
+
 
 # ask first player to choose a number to mark a spot
 
