54c58d70c31552adb1ab34360fd7c3454243a433
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-04-20 18:05:02 -0400

    added winning combinations to game classinitialization as well as player mark parameters and empty arrays to contain all the chosen positions instead of keeping it in the PlayerSet class. | formatting

--- new_main.rb
+++ new_main.rb
@@ -2,62 +2,69 @@
 
 puts "\n***WELCOME TO TIC TAC TOE***"
 
-class PlayerSet # 2 players per game
+# 2 players per game
+class PlayerSet
   attr_accessor :player_one_marks, :player_two_marks
-  attr_reader :player_one_choice, :player_two_choice
+  attr_reader :player_one, :player_two
 
   def initialize
     puts "\nPlayer 1, please enter x or o"
-    @player_one_choice = validate_choice(gets.chomp)
-    @player_two_choice = determine_player_two_choice(@player_one_choice)
-    puts "\nPlayer 1 has chosen #{@player_one_choice}. Player 2 is #{@player_two_choice}."
-    @player_one_marks = []
-    @player_two_marks = []
+    @player_one = validate_choice(gets.chomp)
+    @player_two = determine_player_two_choice(@player_one)
+    puts "\nPlayer 1 has chosen #{@player_one}. Player 2 is #{@player_two}."
   end
 
-  def validate_choice(choice) # get first player's choice and only accept input if it is x or o, non-case sensitive but converts to lowercase
+  def validate_choice(choice)
+    # get first player's choice and only accept input if it is x or o, non-case sensitive but converts to lowercase
     choice.downcase!
-    if choice == 'x' || choice == 'o'
-      return choice
+    if %w[x o].include?(choice)
+      choice
     else
       puts "\nYou can only choose x or o, not '#{choice}'. Please start over."
       exit
     end
   end
 
-  def determine_player_two_choice(player_one_choice) # second player is automatically x or o based on first player's choice
-    if player_one_choice == 'x'
-      return "o"
+  def determine_player_two_choice(player_one) 
+    # second player is automatically x or o based on first player's choice
+    if player_one == 'x'
+      'o'
     else
-      return "x"
+      'x'
     end
   end
-
 end
 
 players = PlayerSet.new
 
-
 # show the game board
 class Game
+  @current_turn = 'x'
   attr_accessor :board
 
-  def initialize
-    @board = ["_1_|_2_|_3_",
-      "_4_|_5_|_6_",
-      " 7 | 8 | 9 "]
+  def initialize(player_one_mark,  player_two_mark)
+    @player_one_mark = player_one_mark
+    @player_two_mark = player_two_mark
+    @board = ['_1_|_2_|_3_', '4_|_5_|_6_', ' 7 | 8 | 9 ']
+    @winning_combinations = [[123], [456], [789], [147], [258], [369], [357], [159]]
+    @x_marks = []
+    @o_marks = []
   end
 
   def place_mark(position, mark)
-    # Place a mark (X or O) on the board at the given position -- 
-    # players.player_one_choice or players.player_two_choice
+    # Place a mark (X or O) on the board at the given position --
+    # mark will be players.player_one or players.player_two
     @board.each do |line|
-      if line.include?("#{position}")
-        spot = line.index("#{position}")
-        line[spot] = "#{mark.upcase}"
+      if line.include?(position.to_s)
+        spot = line.index(position.to_s)
+        line[spot] = mark.upcase.to_s
       end
     end
+    @x_marks << position if mark == 'x'
+    @o_marks << position if mark == 'o'
   end
+
+
 
   def switch_player
 
@@ -78,16 +85,12 @@ class Game
 
 end
 
-
 puts "\nBelow is the game board. To mark a spot, enter the number where you wish to place your mark. Let the games begin! Hey-o!"
 puts "\n"
-
 
 tic_tac_toe = Game.new
 puts tic_tac_toe.board
 puts "\n"
-
-
 
 # ask first player to choose a number to mark a spot
 
@@ -101,12 +104,11 @@ puts "\n"
 
 # if either choice array is at least 3 in length, check for a winner
 
-# CHECKING FOR WINNER: 
+# CHECKING FOR WINNER:
 
 # there are 8 possible winning combinations
 
 # if a choice array has one of the winning combinations, in any order, that player is declared winner and a 'game over, x/o wins' message appears
 
 # if all of the spaces are full and there are no winners, 'game over, tie' message appears
-
 
